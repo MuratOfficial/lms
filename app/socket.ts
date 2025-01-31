c4cc47feca63b33185735c861c340b9fda31434a
MuratOfficial <toimetm@gmail.com>
2025-01-31 10:05:35 +0500

    added env

--- app/socket.ts
+++ app/socket.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("https://replit.com/@MuratOfficial/NumbTestyInsurance");
+const socket: Socket = io(process.env.SOCKET_IO_URL);
 
 export { socket };
